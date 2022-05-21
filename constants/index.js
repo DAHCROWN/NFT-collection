11e4633d24c1a11c47c375413e2ccb0d235e056c
DAHCROWN <dsilakass@gmail.com>
2022-05-21 11:07:36 +0100

    NFT Collection

---
+++ constants/index.js
@@ -0,0 +1,22 @@
+export const abi = [
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "name": "whitelistedAddresses",
+      "outputs": [
+        {
+          "internalType": "bool",
+          "name": "",
+          "type": "bool"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    }
+  ]
+export const NFT_CONTRACT_ADDRESS = 0x3E60599Ccca1172d096FC76Af761fE524bee6e92
